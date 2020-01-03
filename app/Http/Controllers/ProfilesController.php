267f52317a50ad8fb9e18400270fc7d46c9f6b4d
EMMANUEL GABION <emmanuelgabion@EMMANUELs-MacBook-Pro.local>
2020-01-04 01:49:34 +0800

    Added Edit profile functionality

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,12 +7,32 @@ use App\User;
 
 class ProfilesController extends Controller
 {
-    public function index($user_id)
+    public function index(User $user)
     {
-        $user = User::findOrFail($user_id);
-        // dd(  $user->posts   );
-        return view('profiles.index',[
-            'user' => $user,
-        ]);
+        // $user = compact('user');
+        return view('profiles.index', compact('user') );
     }
+
+    public function edit(User $user)
+    { 
+        return view('profiles.edit', compact('user')); 
+    }
+
+    public function update(User $user){
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+
+        // $user->profile->update($data);
+
+        //safer fetch of user
+        auth()->user()->profile->update($data);
+
+        return redirect("/profile/{$user->id}");
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,10 @@ Auth::routes();
 Route:: get('/p/{post}','PostsController@show');
 Route:: get('/p/create', 'PostsController@create');
 Route:: post('/p', 'PostsController@store');
-Route::get('/profile/{id}', 'ProfilesController@index')->name('profile.show');
+
+ Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+// This is the form of the update
+Route::get('/profile/{user}/edit','ProfilesController@edit')->name('profile.edit');
+// This will do the actual update
+Route::patch('/profile/{user}', 'ProfilesController@update' )->name('profile.update');
+
